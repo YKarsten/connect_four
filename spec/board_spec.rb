77fc0ad15edabbc081be40adaaf80c029b61c4a9
YKarsten <yannik.karsten@web.de>
2023-01-29 15:45:58 +0100

    Initialize directory and files

---
+++ spec/board_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require_relative '../lib/board'
+
+describe Board do
+  subject(:board) { described_class.new }
+
+  describe '#method' do
+    context 'some context' do
+      it 'does something' do
+
+        #it end
+      end
+
+      #context end
+    end
+    
+    #describe #method end
+  end
+
+#describe Board end
+end
